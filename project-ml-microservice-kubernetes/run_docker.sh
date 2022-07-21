6ea4255e44765c3e0c4b457825ff8fcf85d59f17
azubuike okom <azubuikeokom@gmail.com>
2022-07-21 15:28:06 +0100

    my first commit

--- project-ml-microservice-kubernetes/run_docker.sh
+++ project-ml-microservice-kubernetes/run_docker.sh
@@ -4,9 +4,12 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+sudo docker build -t ml-housing-price .
 
 # Step 2: 
 # List docker images
+sudo docker images
 
 # Step 3: 
 # Run flask app
+sudo docker run -d -p 8000:80 ml-housing-price
--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -6,10 +6,13 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
-
+export dockerpath=zubyking/ml-housing-price:v1.0.0
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+sudo docker tag ml-housing-price $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+sudo docker login
+sudo docker push $dockerpath
